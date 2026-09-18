68672dfd7e2a2eb3baddc54af984f4302a371506
MRT <mohtaghi6@gmail.com>
2026-09-18 23:21:46 +0330

    Add Main Menu Controller

--- app/src/main/java/peugeot/platform/android/ui/MainMenuController.kt
+++ app/src/main/java/peugeot/platform/android/ui/MainMenuController.kt
@@ -1,1 +1,57 @@
+package peugeot.platform.android.ui
 
+enum class MainMenuPage {
+    HOME,
+    CAR,
+    MUSIC,
+    NAVIGATION,
+    CALL,
+    SCAN
+}
+
+class MainMenuController(
+    private val onPageChanged:
+        (MainMenuPage) -> Unit
+) {
+
+    private var currentPage =
+        MainMenuPage.HOME
+
+    fun open(
+        page: MainMenuPage
+    ) {
+
+        currentPage = page
+
+        onPageChanged(page)
+    }
+
+    fun home() {
+        open(MainMenuPage.HOME)
+    }
+
+    fun car() {
+        open(MainMenuPage.CAR)
+    }
+
+    fun music() {
+        open(MainMenuPage.MUSIC)
+    }
+
+    fun navigation() {
+        open(MainMenuPage.NAVIGATION)
+    }
+
+    fun call() {
+        open(MainMenuPage.CALL)
+    }
+
+    fun scan() {
+        open(MainMenuPage.SCAN)
+    }
+
+    fun getCurrentPage():
+        MainMenuPage {
+        return currentPage
+    }
+}
