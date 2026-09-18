3f210588d61383d0445c646ae8f8d6260d3840e3
MRT <mohtaghi6@gmail.com>
2026-09-18 13:10:06 +0330

    Update ResourceManager.kt

--- app/src/main/java/peugeot/platform/android/core/core/core/core/ResourceManager.kt
+++ app/src/main/java/peugeot/platform/android/core/core/core/core/ResourceManager.kt
@@ -1,12 +1,1 @@
-package peugeot.platform.android.core
 
-object ResourceManager {
-
-    @Volatile
-    var loaded: Boolean = false
-        private set
-
-    fun load() {
-        loaded = true
-    }
-}
