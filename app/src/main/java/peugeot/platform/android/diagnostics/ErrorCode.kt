311ebd929cc4ae004052accb724f84fc9b5cb3dc
MRT <mohtaghi6@gmail.com>
2026-09-19 21:52:03 +0330

    Fix ErrorCode model for Scanner

--- app/src/main/java/peugeot/platform/android/diagnostics/ErrorCode.kt
+++ app/src/main/java/peugeot/platform/android/diagnostics/ErrorCode.kt
@@ -1,14 +1,28 @@
 package peugeot.platform.android.diagnostics
 
+
 data class ErrorCode(
+
     val code: String,
+
+    val title: String,
+
     val description: String,
+
     val severity: Severity,
+
     val active: Boolean = true
+
 )
 
+
+
 enum class Severity {
+
     INFO,
+
     WARNING,
+
     CRITICAL
+
 }
