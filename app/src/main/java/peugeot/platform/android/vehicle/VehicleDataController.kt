868cbeb0d27f28606bff250f5744f292d539ee24
MRT <mohtaghi6@gmail.com>
2026-09-19 21:30:26 +0330

    Refactor VehicleDataController to improve data handling

--- app/src/main/java/peugeot/platform/android/vehicle/VehicleDataController.kt
+++ app/src/main/java/peugeot/platform/android/vehicle/VehicleDataController.kt
@@ -2,39 +2,82 @@ package peugeot.platform.android.vehicle
 
 import peugeot.platform.android.can.CANFrame
 
+
 class VehicleDataController(
     private val onDataChanged: (VehicleData) -> Unit
 ) {
 
+
+    private var lastData: VehicleData? = null
+
+
+
     fun updateFromCAN(
         frame: CANFrame
     ) {
-        VehicleDataManager.updateFromCAN(frame)
 
-        onDataChanged(
-            VehicleDataManager.getData()
+
+        VehicleDataManager.updateFromCAN(
+            frame
         )
+
+
+        publish()
     }
+
+
 
     fun useDemoMode() {
 
+
         VehicleDataManager.setDemoMode()
 
-        onDataChanged(
-            VehicleDataManager.getData()
-        )
+
+        publish()
     }
 
+
+
     fun getCurrentData(): VehicleData {
+
         return VehicleDataManager.getData()
+
     }
+
+
 
     fun reset() {
 
+
         VehicleDataManager.reset()
 
-        onDataChanged(
-            VehicleDataManager.getData()
-        )
+
+        publish()
+
     }
+
+
+
+    private fun publish() {
+
+
+        val data =
+            VehicleDataManager.getData()
+
+
+
+        if (data != lastData) {
+
+
+            lastData = data
+
+
+            onDataChanged(
+                data
+            )
+
+        }
+
+    }
+
 }
