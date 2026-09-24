7b6f82cc4965c92a5cb97b4488dcabcebaee1e76
MRT <mohtaghi6@gmail.com>
2026-09-24 10:01:24 +0330

    Update MainActivity.kt

--- app/src/main/java/peugeot/platform/android/MainActivity.kt
+++ app/src/main/java/peugeot/platform/android/MainActivity.kt
@@ -16,8 +16,7 @@ import peugeot.platform.android.ai.VoiceManager
 
 import peugeot.platform.android.can.CANReceiver
 
-import peuugeot.platform.android.dashboard.BMWMenu
-import peugeot.platform.android.dashboard.Dashboard3DView
+import peugeot.platform.android.dashboard.BMWMenu
 
 import peugeot.platform.android.ui.CallPageView
 import peugeot.platform.android.ui.CarPageView
